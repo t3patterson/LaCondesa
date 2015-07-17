f4f69797a2ab116f9c76b3ee4e1043e2588130cb
Travis H <t3patterson@gmail.com>
2015-07-16 20:01:12 -0500

    handling menu glitch

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -24,7 +24,79 @@ import { MyHomePage } from './bootstrap-react-components.js'
 window.addEventListener('load', app)
 
 function app() {
-	console.log('app loded')
+	var menuEl = document.querySelector('.menu-pop-out')
+	var wrapperEl = document.querySelector('.wrapper')
+	var overlayEl = document.querySelector('#overlay')
+	var revealedEl
+
+
+	var initialHeight = wrapperEl.offsetHeight
+	console.log(initialHeight)
+	console.log(menuEl.offsetHeight-initialHeight-menuEl.offsetTop)
+	var heightDiff = (menuEl.offsetHeight-initialHeight-menuEl.offsetTop)
+	var deadzone = (menuEl.offsetHeight+menuEl.offsetTop-initialHeight)
+	console.log(deadzone)
+
+	var revealAbout = (evt)=>{
+		evt.preventDefault();
+		console.log(evt.target)
+
+		var aboutEl = document.querySelector('.about-us');
+		aboutEl.classList.add('show-me')
+
+		overlayEl.classList.add("show-me");
+		revealedEl = aboutEl;
+	}
+
+	var revealMenu = (evt) => {
+		evt.preventDefault();
+		console.log(evt.target);
+		
+		var menuPopOutEl = document.querySelector('.menu-pop-out');
+		menuPopOutEl.classList.add("show-me");
+		
+		overlayEl.classList.add("show-me");
+		wrapperEl.classList.add("flow-over")
+		console.log(wrapperEl.offsetHeight)
+		revealedEl = menuPopOutEl
+	}
+
+	var revealMap = (evt)=>{
+		evt.preventDefault();
+		console.log(evt.target)
+
+		var mapLocEl = document.querySelector('.map-location')
+		mapLocEl.classList.add("show-me");	
+
+		overlayEl.classList.add("show-me");
+		revealedEl = mapLocEl;
+	}
+
+	document.querySelector('.menu-btn').addEventListener('click', revealMenu)
+	document.querySelector('.menu-btn2').addEventListener('click', revealMenu)
+	document.querySelector('.about-btn').addEventListener('click', revealAbout)
+	document.querySelector('.about-btn2').addEventListener('click', revealAbout)
+	document.querySelector('.map-btn').addEventListener('click', revealMap)
+	document.querySelector('.map-btn2').addEventListener('click', revealMap)
+
+	overlayEl.addEventListener('click', (evt)=>{
+		var wrapperEl = document.querySelector('.wrapper')
+
+		if (wrapperEl.offsetWidth < 420 && evt.pageY > initialHeight - deadzone){
+			console.log('doom scenario...deadzone clicked')
+			var heightDiff = (menuEl.offsetHeight-wrapperEl.offsetHeight-menuEl.offsetTop)
+			wrapperEl.classList.remove("flow-over")
+			wrapperEl.style.paddingTop = heightDiff+'px'
+		}
+		console.log('overlay clickd')
+		wrapperEl.classList.remove("flow-over")
+		revealedEl.classList.remove("show-me")
+		overlayEl.classList.remove("show-me")
+	})
+
+	console.log( document.querySelector('.about-btn2') )
+	console.log( document.querySelector('.about-btn2') )
+
 }
 
 
